9c2e906ebf49228bf3898ca6c7df16d181cff393
sujal baidhya <findsujal2@gmail.com>
2024-08-12 21:44:45 +0545

    cpp

---
+++ cpp/km&m.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class dis
+{
+	private:
+		int km,m;
+	public:
+		void setval()
+		{
+			cout<<"Enter value of km and m:";
+			cin>>km>>m;
+		}
+		dis operator+(const dis &a)
+		{
+			dis tempobj;
+			int rem;
+			rem=(m+a.m)/1000;
+			tempobj.m=(m+a.m)%1000;
+			tempobj.km=km+a.km+rem;
+			return tempobj;
+		}
+		void display()
+		{
+			cout<<"km="<<km<<endl;
+			cout<<"m="<<m;
+		}
+};
+int main()
+{
+	dis A,B,Ans;
+	A.setval();
+	B.setval();
+	Ans=A+B;
+	Ans.display();
+	return 0;
+}
